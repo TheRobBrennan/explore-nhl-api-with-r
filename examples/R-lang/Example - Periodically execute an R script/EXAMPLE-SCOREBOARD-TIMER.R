4bbcef636459453c7d833e4faf5bc61e5eeb5239
Rob Brennan <rob@therobbrennan.com>
2023-03-21 14:18:32 -0700

    Enhance logging to indicate when dataframes are available in R

--- examples/R-lang/Example - Periodically execute an R script/EXAMPLE-SCOREBOARD-TIMER.R
+++ examples/R-lang/Example - Periodically execute an R script/EXAMPLE-SCOREBOARD-TIMER.R
@@ -1,6 +1,7 @@
 THREE_MINUTES_IN_SECONDS <- 60 * 3
 DELAY_IN_SECONDS <- THREE_MINUTES_IN_SECONDS
 EXECUTION_ATTEMPTS <- 1
+EMPTY_SPACES <- "    "
 
 # Load scoreboards
 NHL_SCOREBOARD_SCRIPT <- sprintf("%s/examples/R-lang/National Hockey League - NHL/NHL API - Example to view data for a specific NHL game ID/NHL-API-view-data-for-a-specific-game-id.R", getwd())
@@ -20,14 +21,17 @@ while (TRUE) {
 
   # View the data frame - focused on NHL updates
   if (exists("nhl_schedule_details_games_dataframe_filtered")) {
+    print(paste(EMPTY_SPACES, "-> NHL schedule has loaded at", Sys.time()))
     View(nhl_schedule_details_games_dataframe_filtered)
   }
 
   if (exists("whl_scorebar_dataframe_filtered")) {
+    print(paste(EMPTY_SPACES, "-> WHL scoreboard available at", Sys.time()))
     View(whl_scorebar_dataframe_filtered)
   }
 
   if (exists("nhl_scoreboard_dataframe")) {
+    print(paste(EMPTY_SPACES, "-> NHL scoreboard available at", Sys.time()))
     View(nhl_scoreboard_dataframe)
   }
 
