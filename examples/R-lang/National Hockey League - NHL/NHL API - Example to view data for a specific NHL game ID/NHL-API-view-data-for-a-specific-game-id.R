4f46ab4e230cf0134224a1c03cc79e74eae8deb3
Rob Brennan <rob@therobbrennan.com>
2023-05-15 15:09:07 -0700

    Add tonight's decisive game 7 in Dallas. Let's go Kraken!!!

--- examples/R-lang/National Hockey League - NHL/NHL API - Example to view data for a specific NHL game ID/NHL-API-view-data-for-a-specific-game-id.R
+++ examples/R-lang/National Hockey League - NHL/NHL API - Example to view data for a specific NHL game ID/NHL-API-view-data-for-a-specific-game-id.R
@@ -19,7 +19,7 @@ library(purrr)
 try(
   {
     # Click on an individual game in the scorebar at https://www.nhl.com to get the game ID
-    NHL_GAME_ID <- 2022030236
+    NHL_GAME_ID <- 2022030237
 
     # Build the URL to load our live game data
     NHL_BASE_API_URL <- "https://statsapi.web.nhl.com/api/v1"
