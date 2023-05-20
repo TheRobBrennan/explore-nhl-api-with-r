78b541ef10ac1825a6b07b99691315997f04343e
Rob Brennan <rob@therobbrennan.com>
2023-05-20 14:40:55 -0700

    Consolidate multiple mutate statements into one for nhl_schedule_details_games_dataframe_filtered

--- examples/R-lang/National Hockey League - NHL/NHL API - Example to view data for a specific NHL game ID/NHL-API-view-data-for-a-specific-game-id.R
+++ examples/R-lang/National Hockey League - NHL/NHL API - Example to view data for a specific NHL game ID/NHL-API-view-data-for-a-specific-game-id.R
@@ -51,15 +51,17 @@ try(
       select(gamePk, gameDate, status, teams, linescore) %>%
       unnest(status) %>%
       unnest(teams) %>%
-      # ymd_hms is used to convert the gameDate column in the dataframe to a date-time object
-      mutate(gameDateYMDHMS = ymd_hms(gameDate)) %>% # 2023-05-13 02:00:00
-      mutate(gameDateFormatted = format(
-        # with_tz is used to convert the date-time object to the local time zone - which is "America/Los_Angeles" for Seattle WA
-        with_tz(gameDateYMDHMS, Sys.timezone()),
-        # format is used to format the date-time object to the desired output format ("%Y-%m-%d %I:%M%p %Z") which gives the date and time in the format "YYYY-MM-DD hh:mmAM/PM Timezone"
-        # %I will display a twelve-hour time value with a leading zero. In this case, I want to use %l so we see the desired value
-        format = "%Y-%m-%d %l:%M %p %Z" # 2023-05-12  7:00 PM PDT
-      )) %>%
+      mutate(
+        # ymd_hms is used to convert the gameDate column in the dataframe to a date-time object
+        gameDateYMDHMS = ymd_hms(gameDate), # 2023-05-13 02:00:00
+        gameDateFormatted = format(
+          # with_tz is used to convert the date-time object to the local time zone - which is "America/Los_Angeles" for Seattle WA
+          with_tz(gameDateYMDHMS, Sys.timezone()),
+          # format is used to format the date-time object to the desired output format ("%Y-%m-%d %I:%M%p %Z") which gives the date and time in the format "YYYY-MM-DD hh:mmAM/PM Timezone"
+          # %I will display a twelve-hour time value with a leading zero. In this case, I want to use %l so we see the desired value
+          format = "%Y-%m-%d %l:%M %p %Z" # 2023-05-12  7:00 PM PDT
+        )
+      ) %>%
       select(gamePk, gameDate, gameDateFormatted, everything())
     
     nhl_scoreboard_dataframe <- nhl_schedule_details_games_dataframe_filtered %>%
